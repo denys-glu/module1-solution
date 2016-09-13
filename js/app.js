0c22dd3205529451a1d99b005acde0aa45b2c35e
Denis Glukhovskyy <denis.glukhovskyy@areon.ua>
2016-09-13 13:54:03 +0300

    removed console.logs

--- js/app.js
+++ js/app.js
@@ -9,7 +9,6 @@
 //     $scope.checkLunch = function(list){
 //       //list = list.split(',').filter(str => /\S/.test(str));/*triend arrow function*/
 //       list = list.split(',').filter(function(str){return /\S/.test(str)} );
-//       console.log(list.length);
 //       if(list.length > 3){
 //         tooMuch();
 //       } else if (list.length <= 3 && list.length != 0){
@@ -22,19 +21,16 @@
 //       $scope.changeBorder = "redBorder";
 //       $scope.checkLunchResult = "Please enter data first";
 //       $scope.changeFont = "";
-//       console.log("emptyValue")
 //     }
 //     function tooMuch(){
 //       $scope.checkLunchResult = "Too much!"
 //       $scope.changeFont = "bad";
-//       console.log("tooMuch");
 //     }
 //     function good(){
 //       $scope.changeBorder = "greenBorder";
 //       $scope.checkLunchResult = "Enjoy!";
 //       $scope.changeFont = "good";
-//       console.log("good");
 //     }
 //   };
 // })();
-!function(){"use strict";function e(e){function n(){e.changeBorder="redBorder",e.checkLunchResult="Please enter data first",e.changeFont="",console.log("emptyValue")}function o(){e.checkLunchResult="Too much!",e.changeFont="bad",console.log("tooMuch")}function c(){e.changeBorder="greenBorder",e.checkLunchResult="Enjoy!",e.changeFont="good",console.log("good")}e.listOfFood="",e.checkLunchResult="",e.checkLunch=function(e){e=e.split(",").filter(function(e){return/\S/.test(e)}),console.log(e.length),e.length>3?o():e.length<=3&&0!=e.length?c():n()}}angular.module("LunchCheck",[]).controller("LunchCheckController",e),e.$inject=["$scope"]}();
+!function(){"use strict";function n(n){function e(){n.changeBorder="redBorder",n.checkLunchResult="Please enter data first",n.changeFont=""}function c(){n.checkLunchResult="Too much!",n.changeFont="bad"}function t(){n.changeBorder="greenBorder",n.checkLunchResult="Enjoy!",n.changeFont="good"}n.listOfFood="",n.checkLunchResult="",n.checkLunch=function(n){n=n.split(",").filter(function(n){return/\S/.test(n)}),n.length>3?c():n.length<=3&&0!=n.length?t():e()}}angular.module("LunchCheck",[]).controller("LunchCheckController",n),n.$inject=["$scope"]}();
