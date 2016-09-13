bf697575e852a7ea119e7c89da3f378fc704a3bd
Denis Glukhovskyy <denis.glukhovskyy@areon.ua>
2016-09-13 13:37:02 +0300

    fixed empty text box message

--- js/app.js
+++ js/app.js
@@ -7,22 +7,34 @@
 //     $scope.listOfFood = "";
 //     $scope.checkLunchResult = "";
 //     $scope.checkLunch = function(list){
-//       list = list.split(',').filter(str => /\S/.test(str));/*triend arrow function*/
-//       //list = list.split(',').filter(function(str){return /\S/.test(str)} );
+//       //list = list.split(',').filter(str => /\S/.test(str));/*triend arrow function*/
+//       list = list.split(',').filter(function(str){return /\S/.test(str)} );
+//       console.log(list.length);
 //       if(list.length > 3){
-//         $scope.checkLunchResult = "Too much!"
-//         $scope.changeFont = "bad";
-//       } else {
-//         $scope.checkLunchResult = "Enjoy!"
-//         $scope.changeFont = "good";
-//       }
-//       if($scope.listOfFood != ""){
-//         $scope.changeBorder = "greenBorder";
-//       } else {
-//         $scope.changeBorder = "redBorder";
-//         $scope.checkLunchResult = "";
+//         tooMuch();
+//       } else if (list.length <= 3 && list.length != 0){
+//         good();
+//       } else{
+//         emptyValue();
 //       }
 //     };
+//     function emptyValue(){
+//       $scope.changeBorder = "redBorder";
+//       $scope.checkLunchResult = "Please enter data first";
+//       $scope.changeFont = "";
+//       console.log("emptyValue")
+//     }
+//     function tooMuch(){
+//       $scope.checkLunchResult = "Too much!"
+//       $scope.changeFont = "bad";
+//       console.log("tooMuch");
+//     }
+//     function good(){
+//       $scope.changeBorder = "greenBorder";
+//       $scope.checkLunchResult = "Enjoy!";
+//       $scope.changeFont = "good";
+//       console.log("good");
+//     }
 //   };
 // })();
-!function(){"use strict";function c(c){c.listOfFood="",c.checkLunchResult="",c.checkLunch=function(e){e=e.split(",").filter(function(c){return/\S/.test(c)}),e.length>3?(c.checkLunchResult="Too much!",c.changeFont="bad"):(c.checkLunchResult="Enjoy!",c.changeFont="good"),""!=c.listOfFood?c.changeBorder="greenBorder":(c.changeBorder="redBorder",c.checkLunchResult="")}}angular.module("LunchCheck",[]).controller("LunchCheckController",c),c.$inject=["$scope"]}();
+!function(){"use strict";function e(e){function n(){e.changeBorder="redBorder",e.checkLunchResult="Please enter data first",e.changeFont="",console.log("emptyValue")}function o(){e.checkLunchResult="Too much!",e.changeFont="bad",console.log("tooMuch")}function c(){e.changeBorder="greenBorder",e.checkLunchResult="Enjoy!",e.changeFont="good",console.log("good")}e.listOfFood="",e.checkLunchResult="",e.checkLunch=function(e){e=e.split(",").filter(function(e){return/\S/.test(e)}),console.log(e.length),e.length>3?o():e.length<=3&&0!=e.length?c():n()}}angular.module("LunchCheck",[]).controller("LunchCheckController",e),e.$inject=["$scope"]}();
